d63f180c9fcac1a45d07be41329015723cf70a2c
MacStudent <macstudent@20X-iMacXX-M-481.local>
2018-08-14 17:17:18 -0400

    adding signup nd coding nd design home screen

--- PayrollSystem/LoginViewController.swift
+++ PayrollSystem/LoginViewController.swift
@@ -12,23 +12,23 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var txtUserName: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
-    @IBAction func swRem(_ sender: UISwitch) {
-    }
+    @IBOutlet weak var swRem: UISwitch!
     
-    @IBAction func btLogin(_ sender: UIButton) {
-        if txtUserName.text == "admin@a.com" && txtPassword.text == "SECRET"
-        {
-            performSegue(withIdentifier: "showHomeScreen", sender: self)
-            print("Login Success")
-            
-        }
-        else{
-            print("User Email / Password incorrect")
-        }
-    }
+     var userDefault: UserDefaults?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        userDefault = UserDefaults.standard
+        if let userId = userDefault?.value(forKey: "email")
+        {
+            if let userPassword = userDefault?.value(forKey: "password")
+            {
+                txtUserName.text = userId as? String
+                txtPassword.text = userPassword as? String
+            }
+        }
 
         // Do any additional setup after loading the view.
     }
@@ -37,6 +37,37 @@ class LoginViewController: UIViewController {
   override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+   
+  
+    
+    @IBAction func btLogin(_ sender: UIButton) {
+        if txtUserName.text == "admin@a.com" && txtPassword.text == "SECRET"
+        {
+            if swRem.isOn
+            {
+                userDefault?.set(txtUserName.text, forKey: "email")
+                userDefault?.set(txtPassword.text, forKey: "password")
+            }
+            else  {
+                userDefault?.removeObject(forKey: "email")
+                userDefault?.removeObject(forKey: "password")
+            }
+            performSegue(withIdentifier: "showHomeScreen", sender: self)
+            print("Login Success")
+            
+        }
+        else{
+            print("User Email or Password incorrect")
+        }
+        
+        
+    }
+    
+    @IBAction func btRegister(_ sender: Any) {
+        
+        performSegue(withIdentifier: "showSignUpScreen", sender: self)
     }
     
 
