6af473c4e062ae7467358d4cc292ce669c5bed80
MacStudent <macstudent@20X-iMacXX-M-397.local>
2018-08-17 17:31:26 -0400

    upadted code

--- PayrollSystem/signUPViewController.swift
+++ PayrollSystem/signUPViewController.swift
@@ -13,9 +13,11 @@ class signUPViewController: UIViewController {
     @IBOutlet weak var txtEmail: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
     @IBOutlet weak var txtConfirmPassword: UITextField!
+    var UserDefault: UserDefaults?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
     }
@@ -28,25 +30,60 @@ class signUPViewController: UIViewController {
 
     @IBAction func btRegister(_ sender: Any) {
         
-        let empName = txtEmpName.text
-         let empEmail = txtEmail.text
-         let empPassword = txtPassword.text
-         let empConfirmPassword = txtConfirmPassword.text
+        let empName = txtEmpName;
+        let empEmail = txtEmail;
+        let empPassword = txtPassword;
+        let empConfirmPassword = txtConfirmPassword;
         
-        if ( txtEmpName.isEmpty || txtEmail.isEmpty || txtPassword.isEmpty || txtConfirmPassword.isEmpty){
+       if (txtEmpName.text?.characters.count == 0 || txtEmail.text?.characters.count == 0 || txtPassword.text?.characters.count == 0  || txtConfirmPassword.text?.characters.count == 0 )
+       {
             
-            displayMyAlertMessage("All Fields Mandatory")
+            displayMyAlertMessage(userMessage: "All Fields Mandatory");
             return;
         }
-    }
+        
+        if (empPassword != empConfirmPassword)
+        {
+            displayMyAlertMessage(userMessage: "Password Do not Match");
+            return;
+        }
+        
+        
+        UserDefault = UserDefaults.standard
+        if let empName = UserDefault?.value(forKey: "Name")
+        {
+             txtEmpName.text = empName as? String
+        }
+            if let empPassword = UserDefault?.value(forKey: "Password")
+            {
+               
+                txtPassword.text = empPassword as? String
+            }
+        
+        
+        let MyAlert = UIAlertController(title: "Alert", message: "Register Successfull",preferredStyle: UIAlertControllerStyle.alert);
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default){ ACTION in self.dismiss(animated: true, completion: nil);}
+        
+        MyAlert.addAction(okAction);
+        self.dismiss(animated: true, completion: nil)
+        
+        
     
+        
+       
+    }
     
     func displayMyAlertMessage(userMessage: String)
     {
-        var MyAlert = UIAlertController(title: "Alert", message: userMessage,preferredStyle: UIAlertControllerStyle.alert);
+        let MyAlert = UIAlertController(title: "Alert", message: userMessage,preferredStyle: UIAlertControllerStyle.alert);
         
-        let okAction = UIAlertAction(title: "OK", style:);
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil);
+        
+        MyAlert.addAction(okAction);
+        
+        self.present(MyAlert, animated:true,completion: nil);
     }
+    
     /*
     // MARK: - Navigation
 
